afc78f2d07e934cd851d390eb44383ceecef44bf
olajoao <kusterzito@gmail.com>
2022-06-19 19:17:34 -0300

    fix: api key warning

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 
-// need to exist in .env file
-// i put here only for the challenge
+/**
+* apiKey need to be in .env file
+* i put here only for the challenge 
+* and because i have another one :)
+*/
+
 export const apiKey = "10a06b62438ca5d322f7859e44a15aa5";
 
 import "./index.css";
